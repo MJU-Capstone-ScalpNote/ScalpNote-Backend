7fd1edaf19b86fb464391179eb4a17c3cd69f8e4
LeeJaeHyeok97 <hazardous10@naver.com>
2024-04-19 00:06:52 +0900

    feat: ApiResponse, ControllerAdvice 추가, 시큐리티 초기설정

---
+++ src/main/java/com/scalpnote/global/config/security/CustomAuthenticationProvider.java
@@ -0,0 +1,26 @@
+package com.scalpnote.global.config.security;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+}
