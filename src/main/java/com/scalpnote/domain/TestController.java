569fbce130c0edfaafe0ff60705b1e73c06674a3
이재혁 <67510260+LEEJaeHyeok97@users.noreply.github.com>
2024-04-19 00:08:08 +0900

    feat: ApiResponse, ControllerAdvice 추가, 시큐리티 초기설정 (#5)

--- src/main/java/com/scalpnote/domain/TestController.java
+++ src/main/java/com/scalpnote/domain/TestController.java
@@ -1,7 +1,11 @@
 package com.scalpnote.domain;
 
+import com.scalpnote.domain.user.domain.User;
+import com.scalpnote.global.payload.Message;
+import com.scalpnote.global.payload.ResponseCustom;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +15,10 @@ public class TestController {
     public ResponseEntity<?> test() {
         return ResponseEntity.ok("Hello!!");
     }
+
+    @GetMapping("/test2")
+    public ResponseCustom<?> test2() {
+        return ResponseCustom.OK(new Message("a"));
+    }
+
 }
