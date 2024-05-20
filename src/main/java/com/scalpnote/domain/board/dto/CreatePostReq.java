2fa38f724e59fce439170f65bbe7fa356f634547
이재혁 <67510260+LEEJaeHyeok97@users.noreply.github.com>
2024-05-20 20:37:37 +0900

    feat: 게시글 생성 시 이미지 업로드 구현 (#26) (#36)

--- src/main/java/com/scalpnote/domain/board/dto/CreatePostReq.java
+++ src/main/java/com/scalpnote/domain/board/dto/CreatePostReq.java
@@ -7,13 +7,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Date;
+import java.util.List;
 
 @Getter
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
 public class CreatePostReq {
 
     @Schema(type = "string", example = "제 머리 상태 어떤가요?", description = "게시글 제목")
@@ -22,6 +23,9 @@ public class CreatePostReq {
 
     @Schema(type = "string", example = "지루성 두피염이 나았는데 상태가 어떤가요?", description = "게시글 내용")
     private String content;
+
+    @Schema(type = "string", description = "게시글에 첨부할 이미지 파일", format = "binary")
+    private MultipartFile image;
 
 
 }
