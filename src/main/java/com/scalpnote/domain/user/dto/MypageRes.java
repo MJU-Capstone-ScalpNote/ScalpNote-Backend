c68af1588c32a10b404ac7dd8ba477ff386a1a35
LeeJaeHyeok97 <hazardous10@naver.com>
2024-05-27 01:00:15 +0900

    두피 진단 결과 저장 엔터티 추가, 마이페이지 조회 기능 수정

--- src/main/java/com/scalpnote/domain/user/dto/MypageRes.java
+++ src/main/java/com/scalpnote/domain/user/dto/MypageRes.java
@@ -1,29 +1,67 @@
 package com.scalpnote.domain.user.dto;
 
+import com.scalpnote.domain.user.domain.Model6Result;
 import com.scalpnote.domain.user.domain.Role;
 import com.scalpnote.domain.user.domain.User;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class MypageRes {
     private Long id;
     private String email;
     private String name;
-    private String scalpCondition;
+    private Model6ResultRes model6ResultRes;
     private Role role;
 
+    @Builder
+    public MypageRes(Long id, String email, String name, Model6ResultRes model6ResultRes, Role role) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+        this.model6ResultRes = model6ResultRes;
+        this.role = role;
+    }
 
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Model6ResultRes {
+        private List<Model6ResultDto> model6ResultList = new ArrayList<>();
+    }
 
-    public static MypageRes toDto(User user) {
-        MypageRes mypageRes = MypageRes.builder()
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Model6ResultDto {
+        private Long id;
+        private String scalpCondition;
+        private String createdAt;
+    }
+
+    public static MypageRes toDto(User user, List<Model6Result> model6ResultList) {
+        List<Model6ResultDto> model6ResultDtos = new ArrayList<>();
+        for (Model6Result result : model6ResultList) {
+            model6ResultDtos.add(Model6ResultDto.builder()
+                    .id(result.getId())
+                    .scalpCondition(result.getScalpCondition())
+                    .createdAt(result.getCreatedAt().toString())
+                    .build());
+        }
+
+        return MypageRes.builder()
                 .id(user.getId())
                 .email(user.getEmail())
                 .name(user.getName())
-                .scalpCondition(user.getScalpCondition())
+                .model6ResultRes(Model6ResultRes.builder()
+                        .model6ResultList(model6ResultDtos)
+                        .build())
+                .role(user.getRole())
                 .build();
-        return mypageRes;
     }
 }
