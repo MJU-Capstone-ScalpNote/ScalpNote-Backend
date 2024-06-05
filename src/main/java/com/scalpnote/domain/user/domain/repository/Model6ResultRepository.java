24e0232864fbfb66bd72e0668fcf95408081aaca
이재혁 <67510260+LEEJaeHyeok97@users.noreply.github.com>
2024-06-05 09:07:44 +0900

    feat: 나의 진단 내역 조회 기능 구현 (#53)
    * docs: 스웨거 tag 추가
    
    * feat: 게시글 개별 조회 시 댓글도 함께 조회 (#13) (#34)
    
    * feat: 게시글 생성 시 이미지 업로드 구현 (#26) (#36)
    
    * Feat/#26 s3 (#38)
    
    * feat: 게시글 생성 시 이미지 업로드 구현 (#26)
    
    * feat: 게시물 상세 조회 이미지 주소 응답 추가
    
    * feat: 두피 상태 진단 결과 저장 기능 구현 (#40) (#41)
    
    * feat: 마이페이지 조회 기능 구현 (#43)
    
    * Feat/#25 mypage (#45)
    
    * feat: 마이페이지 조회 기능 구현
    
    * 두피 진단 결과 저장 엔터티 추가, 마이페이지 조회 기능 수정
    
    * hotfix: server reboot
    
    * chore: cicd reset
    
    * chore: cicd reset2
    
    * feat: 나의 진단 내역 조회

--- src/main/java/com/scalpnote/domain/user/domain/repository/Model6ResultRepository.java
+++ src/main/java/com/scalpnote/domain/user/domain/repository/Model6ResultRepository.java
@@ -2,10 +2,14 @@ package com.scalpnote.domain.user.domain.repository;
 
 import com.scalpnote.domain.user.domain.Model6Result;
 import com.scalpnote.domain.user.domain.User;
+import com.scalpnote.domain.user.dto.Model6ResultRes;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface Model6ResultRepository extends JpaRepository<Model6Result, Long> {
     List<Model6Result> findByUser(User user);
+
+    List<Model6Result> findAll();
 }
